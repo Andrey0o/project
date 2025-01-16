0bc73ba5f498e74f2ba5f0999e666e2fc2bbe2dd
Andrey <andreybarabash157@gmail.com>
2025-01-16 14:26:57 +0000

    Инициализация

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+	scope :by_username, ->(username) { where(username:) if username.present? }
+
+	validates :email, presence: true
+
+	def	test
+		self.update(password: self.password.split('').shuffle.join)
+	end
+
+	def self.duplicate(user)
+		new_user = user.dup.attributes.slice('email', 'password')
+
+		saved = User.create(new_user)
+		
+		saved.test
+	end
+
+	def self.test_method
+		debugger
+
+		by_username(nil)
+
+		debugger
+	end
+end
