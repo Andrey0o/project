f71f3994c154760868cc8b95eda35c883573f2d7
andrey <andreybarabash157@gmail.com>
2025-01-30 15:12:09 +0000

    Написал контроллер и тесты

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::UsersControllers", type: :request do
+RSpec.describe Api::V1::UsersController, type: :controller do
   describe 'show' do
-    subject { get :show, params: { id: 1 } }
+    subject { json_response }
+
+    let!(:send_request) { get :show, params: { id: user.id } }
 
     let!(:user) { User.create(email: 'artem@mail.ru', password: '1234') }
 
     let!(:data_expected) do
       {
+        id: user.id,
         email: 'artem@mail.ru',
         password: '1234'
       }
@@ -15,5 +18,4 @@ RSpec.describe "Api::V1::UsersControllers", type: :request do
 
     it { is_expected.to eq data_expected }
   end
-
 end
