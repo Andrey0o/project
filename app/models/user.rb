0ab3a555faed5c5203c68911ee96245fa610f42e
andrey <andreybarabash157@gmail.com>
2025-02-06 15:24:29 +0000

    добавили девайс

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,23 +2,10 @@
 
 # Основная запись приложения
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :trackable
+
   scope :by_username, ->(username) { where(username:) if username.present? }
 
   validates :email, presence: true
-
-  def	test
-    update(password: password.chars.shuffle.join)
-  end
-
-  def self.duplicate(user)
-    new_user = user.dup.attributes.slice('email', 'password')
-
-    saved = User.create(new_user)
-
-    saved.test
-  end
-
-  def self.test_method
-    by_username(nil)
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_19_144924) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_06_152017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
-    t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "username"
+    t.string "encrypted_password", default: "", null: false
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "last_sign_in_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
